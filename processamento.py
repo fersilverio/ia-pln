10453eaa6dbc67e7b272a5732558af3b09defe18
fersilverio <ssilveriofernando45@gmail.com>
2020-11-20 19:04:01 -0300

    Começando cm poucas palavras

---
+++ processamento.py
@@ -0,0 +1,24 @@
+import regex as re
+import numpy as np
+
+# Deixa toda a string para letras minusculas
+def _toLower (string):
+    return string.lower()
+
+# Remove as pontuações das tags
+def _removeTags (string):
+    print('remove tags')
+
+# Remoção de palavras-vazias (muito comuns)
+def _removeEmptyWords (string):
+    print('remove palavras vazias')
+
+# Tokenização
+def _tokenize (string):
+    print('transformando em token')
+
+# Normalização
+def _normalize (string):
+    print('normalizando')
+
+
