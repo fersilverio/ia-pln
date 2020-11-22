352385199b2214477160beb463426e6b7232766a
fersilverio <ssilveriofernando45@gmail.com>
2020-11-22 19:12:26 -0300

    ok

--- processamento.py
+++ processamento.py
@@ -1,24 +1,73 @@
 import regex as re
 import numpy as np
+from bs4 import BeautifulSoup
+import unidecode
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 
 # Deixa toda a string para letras minusculas
 def _toLower (string):
-    return string.lower()
+    string = string.lower()
+    return string
 
-# Remove as pontuações das tags
-def _removeTags (string):
-    print('remove tags')
+# Remove as acentos , cedilhas e similares
+def _removeAcentos (string):
+    string = unidecode.unidecode(string)
+    return string
 
-# Remoção de palavras-vazias (muito comuns)
-def _removeEmptyWords (string):
-    print('remove palavras vazias')
+#Remove a pontuacao
+def _removePontuacao (string):
+    string = re.sub('[!.,;?@#$%&:()^/|]', '', string)
+    return string
+
+# Remove as tags html
+def _removeHtml (string):
+    soup = BeautifulSoup(string, "html.parser")
+    string = soup.get_text() 
+    return string
+
+# Remove os brackets e seu conteudo interno
+def _removeBracketsContent(string):
+    string = re.sub('\[[^]]*\]', '', string) 
+    return string
+
 
 # Tokenização
 def _tokenize (string):
-    print('transformando em token')
+    tokenized_string = string.split()
+    return tokenized_string
+
+
+
+# Remoção de palavras-vazias (muito comuns)
+def _removeStopWords (string):
+    print('remove palavras vazias')
 
 # Normalização
 def _normalize (string):
     print('normalizando')
 
 
+
+def text_processing (string):
+    string = _removeHtml(string)
+    string = _toLower(string)
+    string = _removeAcentos(string)
+    string = _removePontuacao
+    string = _removeBracketsContent(string)
+    return string
+
+
+
+
+string = input()
+'''
+print(string)
+#a = text_processing(string)
+a = _removePontuacao(string)
+print(a)
+'''
+
+a = _tokenize(string)
+print(a)
